ad9df366942a3fea6506c0ebd7bb3d5e26791bda
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2022-09-22 00:28:54 +0200

    fgfdg

---
+++ asd-unitn-template/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Delete these two lines if you use standard input output
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
