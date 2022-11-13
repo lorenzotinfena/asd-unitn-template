efb02362b3224018f802e7a36632febafc5a7422
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2022-11-13 23:16:16 +0100

    cfv

--- asd-unitn-template/main.cpp
+++ asd-unitn-template/main.cpp
@@ -3,9 +3,12 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
     // Delete these two lines if you use standard input output
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
-    
+    // CODE HERE
 }
